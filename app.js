d55261b978abcebe6f88b913cdd5303fb47a56b7
grassfinn <theharper@live.com>
2022-07-05 21:58:41 -0400

    Styling and refactoring

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// https://stackoverflow.com/questions/69346491/how-do-i-use-array-each-with-async-await
 const userInput = document.getElementById('user-input');
 const searchButton = document.getElementById('search');
 const main = document.querySelector('main');
@@ -14,9 +15,7 @@ async function fetchData(userSearch) {
   );
   const data = await response.json();
   searchOutput = data.Search;
-  // console.log(searchOutput);
   searchOutput.forEach((item) => movieId.push(item.imdbID));
-  // console.log(movieId)
 
   // use await and promise.all to loop over all the promises and what ever is needed
   await Promise.all(
@@ -25,37 +24,45 @@ async function fetchData(userSearch) {
         `http://www.omdbapi.com/?i=${item}&type=movie&plot=short&apikey=d63e1fcd`
       );
       const data = await response.json();
-      if (data.Poster !== 'N/A' && data.Plot !== 'N/A') {
+      if (data.Poster !== 'N/A' && data.Plot !== 'N/A' && data.Ratings.length !== 0) {
         movieInfo.push(await data);
       }
     })
   );
-  // console.log('Complete Movie info!', movieInfo);
+  console.log(movieInfo)
   return movieInfo;
 }
 
 // display the info on the dom
 function renderData(arr) {
   arr.map((movie) => {
+    const article = document.createElement('article')
     const div = document.createElement('div')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
+    const p2 = document.createElement('p')
+    const span = document.createElement('span')
     const img = document.createElement('img')
     const button = document.createElement('button')
-    main.append(div)
+    main.append(article)
+    article.appendChild(div)
+    article.setAttribute("class", "movie-card")
+    article.append(img)
     div.append(h3)
-    h3.textContent = movie.Title
-    div.append(img)
-    img.setAttribute("src", `${movie.Poster}`)
+    div.append(p2)
+    // div.append(span)
     div.append(p)
+    div.setAttribute('class', 'movie-info')
+    h3.textContent = movie.Title
+    img.setAttribute("src", `${movie.Poster}`)
+    p2.innerHTML = `${movie.Runtime} <span>${movie.Ratings[0].Value}</span>`
     p.textContent = movie.Plot
+    // span.textContent = movie.Ratings[0].Value
     div.append(button)
     button.textContent = 'Add to Favorites!'
 
     button.addEventListener('click', () => {
-      // console.log(movie)
       favoriteMovies.push(movie)
-      // console.log(favoriteMovies)
     })
     })
 
